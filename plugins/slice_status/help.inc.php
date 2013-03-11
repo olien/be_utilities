e6633974db155a9df6b50e032971e8751a0c5d3f
RexDude <rexdude@arcor.de>
2013-03-11 22:19:05 +0100

    Plugins removed: favicon, slice_status, rex_codemirror

--- plugins/slice_status/help.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-// show plugin description
-echo rex_plugin_factory::getPluginDescription('be_utilities', 'slice_status');
-?>
-<br />
--- plugins/slice_status/settings.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-// --- DYN
-$REX['ADDON']['slice_status']['ajax_mode'] = "1";
-$REX['ADDON']['slice_status']['menuitem_position'] = "right";
-// --- /DYN
