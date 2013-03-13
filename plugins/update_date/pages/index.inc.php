63f89c9f97fb83ec99531786e9a6ad60102e1c53
RexDude <rexdude@arcor.de>
2013-03-13 10:53:33 +0100

    [update_date] Added lang strings

--- plugins/update_date/pages/index.inc.php
+++ plugins/update_date/pages/index.inc.php
@@ -1,19 +1,19 @@
 <?php
 $codeExample = '<?php 
-// Standardausgabe des aktuellen Update Datums: ' . rex_update_date::getDate() . '
+// ' . $I18N->msg('update_date_comment1') . ': ' . rex_update_date::getDate() . '
 echo rex_update_date::getDate();
 
-// Benutzerdefinierte Ausgabe des aktuellen Update Datums: ' . rex_update_date::getDate('m-d-Y') . '  
+// ' . $I18N->msg('update_date_comment2') . ': ' . rex_update_date::getDate('m-d-Y') . '  
 echo rex_update_date::getDate(\'m-d-Y\'); 
 ?>';
 ?>
 
 <div class="rex-addon-output">
-	<h2 class="rex-hl2">Hilfe</h2>
+	<h2 class="rex-hl2"><?php echo $I18N->msg('be_utilities_help_caption'); ?></h2>
 	<div class="rex-area-content">
-		<p class="info-msg">Codebeispiele:</p>
+		<p class="info-msg"><?php echo $I18N->msg('update_date_intro'); ?></p>
 		<p class="info-msg"><?php rex_highlight_string($codeExample); ?></p>
-		<p class="info-msg">Das Datum kann über die PHP-üblichen Formatierungsoptionen formatiert werden:<br /><a class="extern" href="http://php.net/manual/de/function.date.php">http://php.net/manual/de/function.date.php</a></p>
+		<p class="info-msg"><?php echo $I18N->msg('update_date_php_docs_info'); ?>:<br /><a class="extern" href="http://php.net/manual/de/function.date.php">http://php.net/manual/de/function.date.php</a></p>
 	</div>
 </div>
 
