cc3457e239c6d939351a9423e0bdb6ea9558492c
RexDude <rexdude@arcor.de>
2013-04-08 15:59:56 +0200

    [category_separator] fixed ep call condition

--- plugins/category_separator/config.inc.php
+++ plugins/category_separator/config.inc.php
@@ -11,7 +11,7 @@ if ($REX['REDAXO']) {
 	include($REX['INCLUDE_PATH'] . '/addons/be_utilities/plugins/category_separator/classes/class.rex_category_separator.inc.php');
 	include($REX['INCLUDE_PATH'] . '/addons/be_utilities/plugins/category_separator/settings.inc.php');
 
-	if (($REX['ADDON']['category_separator']['hide_cat_id'] != '') && (rex_request('page') == 'structure') && (rex_request('category_id') == '0')) { // only for root cats
+	if (($REX['ADDON']['category_separator']['hide_cat_id'] != '') && (rex_request('page') == 'structure' && rex_request('category_id') == '' || rex_request('category_id') == '0')) { // only for root cats
 		rex_register_extension('OUTPUT_FILTER', 'rex_category_separator::appendToBody');
 	}
 }
