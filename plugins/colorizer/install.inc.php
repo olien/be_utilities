e3166a4f27c9c30be964289eff2a584309b3ab82
RexDude <rexdude@arcor.de>
2013-09-23 15:18:13 +0200

    Fixed notice

--- plugins/colorizer/install.inc.php
+++ plugins/colorizer/install.inc.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($ADDONSsic) && $ADDONSsic['status']['website_manager']) { // workaround for OOAdon::isAvailable() for plugins
+if(isset($ADDONSsic) && isset($ADDONSsic['status']['website_manager']) && $ADDONSsic['status']['website_manager']) { // workaround for OOAdon::isAvailable() for plugins
 	$REX['ADDON']['installmsg']['colorizer'] = $I18N->msg('be_utilities_uninstall_website_manager'); 
 } else {
   $REX['ADDON']['install']['colorizer'] = true;
