a675f70a8bd839e2dde6e9a7972e0e34d70e6cf4
RexDude <rexdude@arcor.de>
2013-12-05 15:55:40 +0100

    Better hide start article

--- plugins/hide_startarticle/classes/class.rex_hide_startarticle.inc.php
+++ plugins/hide_startarticle/classes/class.rex_hide_startarticle.inc.php
@@ -4,15 +4,21 @@ class rex_hide_startarticle {
 	static function hideStartArticle($params) {
 		global $REX;
 
+		$params['subject'] = str_replace('<tr>
+              <td class="rex-icon"><a class="rex-i-element rex-i-article-startpage"', '<tr id="rex-structure-startpage">
+              <td class="rex-icon"><a class="rex-i-element rex-i-article-startpage"', $params['subject']);
+
 		$insert = '
 			<!-- hide_startarticle -->
 			<style type="text/css">
-				.rex-table:not(.rex-table-mrgn) tbody tr:first-child { display: none; }
+				tr#rex-structure-startpage { display: none; }
 				li.rex-linkmap-startpage { display: none; }
 			</style>
 			<!-- hide_startarticle -->
 		';
+
+		$params['subject'] = str_replace('</head>', $insert . '</head>', $params['subject']);
 		
-		return $params['subject'] . PHP_EOL . $insert;
+		return $params['subject'];
 	}
 }
