5a064d70f54bb8dd510ff60dd47f5d2016405d1f
RexDude <rexdude@arcor.de>
2015-10-20 00:22:36 +0200

    colorpicker in backend available 2

--- plugins/colorizer/files/init.js
+++ plugins/colorizer/files/init.js
@@ -1,16 +1,20 @@
 jQuery(document).ready( function() {
-	jQuery(".rex-colorpicker").ColorPicker({
-		onSubmit: function(hsb, hex, rgb, el) {
-			jQuery(el).ColorPickerHide();
-		},
-		onBeforeShow: function () {
+	jQuery(".rex-colorpicker").each(function() {
+		var colorPickerTextfieldId = jQuery(this).attr('id');
+
+		jQuery(jQuery(this)).ColorPicker({
+			onSubmit: function(hsb, hex, rgb, el) {
+				jQuery(el).ColorPickerHide();
+			},
+			onBeforeShow: function () {
+				jQuery(this).ColorPickerSetColor(this.value);
+			},
+			onChange: function (hsb, hex, rgb) {
+				jQuery('#' + colorPickerTextfieldId).val("#" + hex);
+			}
+		})
+		.bind("keyup", function(){
 			jQuery(this).ColorPickerSetColor(this.value);
-		},
-		onChange: function (hsb, hex, rgb) {
-			jQuery(this).val("#" + hex);
-		}
-	})
-	.bind("keyup", function(){
-		jQuery(this).ColorPickerSetColor(this.value);
+		});
 	});
 });
